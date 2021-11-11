d57013f2c3e05ecd936fa469ec503bd8acaf9aaa
Natalia <u201911485@upc.edu.pe>
2021-11-10 19:12:13 -0500

    nav bar select

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,33 +11,58 @@ var btn_open = document.getElementById("btn_open");
 var body = document.getElementById("body");
 
 //Evento para mostrar y ocultar menú
-    function open_close_menu(){
-        body.classList.toggle("body_move");
-        side_menu.classList.toggle("menu__side_move");
-    }
+function open_close_menu() {
+	body.classList.toggle("body_move");
+	side_menu.classList.toggle("menu__side_move");
+}
 
 //Si el ancho de la página es menor a 760px, ocultará el menú al recargar la página
 
-if (window.innerWidth < 760){
+if (window.innerWidth < 760) {
 
-    body.classList.add("body_move");
-    side_menu.classList.add("menu__side_move");
+	body.classList.add("body_move");
+	side_menu.classList.add("menu__side_move");
 }
+
+//selected
+var noSelectContainer = document.getElementById("opciones_menu");
+
+var noSelect = noSelectContainer.getElementsByClassName("noSelect");
+
+for (var i = 0; i < noSelect.length; i++) {
+	noSelect[i].addEventListener("click", function() {
+	var current = document.getElementsByClassName("selected");
+	
+	if(current.length > 0 ){
+	current[0].className = current[0].className.replace(" selected", "");
+		}
+	
+		this.className += " selected";
+	});
+}
+
+
+
+
+
+
 
 //Haciendo el menú responsive(adaptable)
 
-window.addEventListener("resize", function(){
+window.addEventListener("resize", function() {
 
-    if (window.innerWidth > 760){
+	if (window.innerWidth > 760) {
 
-        body.classList.remove("body_move");
-        side_menu.classList.remove("menu__side_move");
-    }
+		body.classList.remove("body_move");
+		side_menu.classList.remove("menu__side_move");
+	}
 
-    if (window.innerWidth < 760){
+	if (window.innerWidth < 760) {
 
-        body.classList.add("body_move");
-        side_menu.classList.add("menu__side_move");
-    }
+		body.classList.add("body_move");
+		side_menu.classList.add("menu__side_move");
+	}
+
+
 
 });
