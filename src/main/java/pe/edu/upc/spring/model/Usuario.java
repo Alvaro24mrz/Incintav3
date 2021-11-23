22eb3743e19c411a7e73ec969e20b2d3d64c8797
Natalia Villanueva <villanuevaznatalia@gmail.com>
2021-11-23 02:03:20 -0500

    si funciona vivo
    yay

--- src/main/java/pe/edu/upc/spring/model/Usuario.java
+++ src/main/java/pe/edu/upc/spring/model/Usuario.java
@@ -13,7 +13,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotEmpty;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
